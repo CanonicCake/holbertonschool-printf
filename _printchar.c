7d36af9335a83ce3e2c546280dcfd82f583826b3
CanonicCake <n.a.ellis99@gmail.com>
2023-07-13 10:42:32 -0700

    changes?

--- _printchar.c
+++ _printchar.c
@@ -6,44 +6,44 @@
 #include "main.h"
 
 /**
- * printchar - prints a character
+ * _printchar - prints a character
  * @arg: character to print
  * Return: character asked to print
  */
 
 int _printchar(va_list arg)
 {
-	char c = va_arg (arg, int);
+	char c = va_arg(arg, int);
 
 	_putchar (c);
 
-	va_end (arg);
+	va_end(arg);
 	return (1);
 }
 
 
 
 /**
- * printstr - prints a string of characters
+ * _printstr - prints a string of characters
  * @arg: string to print
  * Return: characters in a string
  */
 
 int _printstr(va_list arg)
 {
-	char *string = va_arg (arg, char*);
+	char *string = va_arg(arg, char*);
 	int i = 0;
 
 	if (string == NULL)
 		string = "null";
 	for (; string[i]; i++)
 		_putchar (string[i]);
-	va_end (arg);
+	va_end(arg);
 	return (i);
 }
 
 /**
- * printprec - prints a %
+ * _printperc - prints a %
  * @arg: % to print
  * Return: %
  */
@@ -51,10 +51,11 @@ int _printstr(va_list arg)
 int _printperc(va_list arg)
 {
 	char perc = 37;
-	perc = va_arg (arg, int);
+
+	perc = va_arg(arg, int);
 
 	_putchar(perc);
 
-	va_end (arg);
-	return(1);
+	va_end(arg);
+	return (1);
 }
