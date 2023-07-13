cffae371de50ac94719c20f28dc9783f098b32ea
Pilar Thornbrugh <pilarthornbrugh@Pilars-MacBook-Pro.local>
2023-07-13 11:03:28 -0500

    adding format to _printf.c

--- _printf.c
+++ _printf.c
@@ -6,36 +6,47 @@
 /**
  * _printf - prints output based on format
  * @format: format specifier
- * Return: number of characters
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-    va_list arg;
-    int i, print_chars = 0;
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	va_list arg;
+	int i, print_chars = 0;
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
         return(-1);
-    va_start(arg, format);
-    for (i=0; format && format[i]; i++)
-    {
-        if(format[i] != '%')
-        {
-            _putchar(format[i]);
-            print_chars++;
-        }
-        else if (format[i + 1] == '\0')
-            return(-1);
-        else if (format[i + 1] == '%')
-        {
-            _putchar(format[i]);
-            print_chars++;
-            i++;
-        }
-        else
-        {
-            _putchar(format[i]);
-            print_chars++;
-        }
-    }
-    va_end(arg);
-    return (print_chars);
+	
+	va_start(arg, format);
+ 	for (i=0; format && format[i]; i++)
+	{
+
+		if(format[i] != '%')
+        	{
+			_putchar(format[i]);
+			print_chars++;
+		}
+
+		else if (format[i + 1] == '\0')
+			return(-1);
+
+		else if (format[i + 1] == '%')
+		{
+			_putchar(format[i]);
+			print_chars++;
+			i++;
+		}
+
+		else if(prnt_frmt(format[i + 1]) != NULL)
+		{
+			print_chars = print_chars +print_frmt(format[ i +])(arg);
+		}
+
+		else
+  		{
+ 			_putchar(format[i]);
+			print_chars++;
+		}
+	}
+	va_end(arg);
+	return (print_chars);
 }
