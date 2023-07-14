2cae3334f37b5cfc41c824eba37aea9a60db1cac
CanonicCake <n.a.ellis99@gmail.com>
2023-07-14 09:41:52 -0700

    changed main.h

--- main.h
+++ main.h
@@ -5,15 +5,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct format_h
+{
+	char *type;
+	int(*f)(va_list);
+}prnt_frmt;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _printchar(va_list arg);
 int _printstr(va_list arg);
 int _printint(va_list arg);
 int _printprec(va_list arg);
-typedef struct format_h
-{
-	char *type;
-	int(*f)(va_list);
-}prnt_frmt;
+
 #endif
